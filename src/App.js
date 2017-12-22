7df7f8cfebe0b139a013edaad38b5b067c7bd55d
Alex Wilmer <djazium@gmail.com>
2017-12-22 18:12:37 -0500

    yay

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactRevealText from 'react-reveal-text'
+import './App.css'
 
 class App extends Component {
+  state = { show: false, showHeart: false, showImage: false }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true })
+    }, 1000)
+    setTimeout(() => {
+      this.setState({ showHeart: true })
+    }, 3000)
+    setTimeout(() => {
+      this.setState({ showImage: true })
+    }, 5000)
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={`App ${this.state.showImage}`}>
+        <div
+          className={`bg-image ${this.state.showImage}`}
+          style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/us.jpg)` }}
+        />
+        <ReactRevealText
+          show={this.state.show}
+          className={`reveal ${this.state.showImage}`}
+        >
+          Merry&nbsp;Christmas&nbsp;Laura
+        </ReactRevealText>
+        <div className={`reveal-heart ${this.state.showHeart}`}>💜🎄⭐️</div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
